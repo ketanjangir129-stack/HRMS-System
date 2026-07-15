1b326907d2b91c9a682669e5b46e0108b73dbdb8
Govind <govindmeena.wevois@gmail.com>
2026-07-15 17:01:11 +0530

    updated the dashboard ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Dashboard from "./pages/Dashboard";
+// import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
-createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AuthProvider>
+      {/* <App /> */}
+      <Dashboard />
+    </AuthProvider>
+  </StrictMode>
+);
 
-)
