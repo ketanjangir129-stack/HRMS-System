e734fdf2195e4a8d6ed51fdd2c5a6fff21c861f7
IamHarru <mayanksharma20004@gmail.com>
2026-07-16 09:14:47 +0530

    initial updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Register from "./pages/authenticate/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/authenticate/login";
+import Register from "./pages/authenticate/register";
+import Dashboard from "./pages/Dashboard";
 
-function App(){
-  return(
-    <>
-      <Register /> 
-    </>
-  )
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "./index.css";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App.jsx'
-import { AuthProvider } from './context/AuthContext.jsx'
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <App />
-    <ToastContainer position="bottom-right" />
-  </AuthProvider>
-
-)
+import "./index.css"
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+      <ToastContainer position="bottom-right" />
+    </AuthProvider>
+  </React.StrictMode>
+);
