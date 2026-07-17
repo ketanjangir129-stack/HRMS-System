d0fb959548524c848918f871885f73b9ff6cbac5
Bhumika Gautam <bhumikagautammitcse@gmail.com>
2026-07-17 11:11:07 +0530

    update employee detail page

--- src/pages/EmployeesDetails.jsx
+++ src/pages/EmployeesDetails.jsx
@@ -1,16 +1,70 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getEmployeeById } from "../services/EmployeeService";
-function EmployeesDetails(){
+function EmployeesDetails() {
     const companyCode = localStorage.getItem("companyCode");
+    const { id } = useParams();
     const [employee, setEmployee] = useState(null);
-    const {id} = useParams();
-    return(
+    useEffect(() => {
+        loadEmployee();
+    }, []);
+
+    const loadEmployee = async () => {
+
+        const data = await getEmployeeById(
+            companyCode,
+            id
+        );
+
+        setEmployee(data);
+    };
+    if (!employee) {
+        return <h2>Loading...</h2>;
+        { employee.employeeId }
+        { employee.name }
+        { employee.email }
+        { employee.mobile }
+        { employee.department }
+        { employee.designation }
+        { employee.address }
+    }
+
+    return (
         <div className="bg-white p-8 rounded-xl shadow">
             <h1 className="text-3xl font-bold mb-6">Employee Details</h1>
 
-            <p>Employee details will come here...</p>
-            
+            <div className="space-y-4">
+
+                <p>
+                    <strong>Employee ID :</strong> {employee.employeeId}
+                </p>
+
+                <p>
+                    <strong>Name :</strong> {employee.name}
+                </p>
+
+                <p>
+                    <strong>Email :</strong> {employee.email}
+                </p>
+
+                <p>
+                    <strong>Mobile :</strong> {employee.mobile}
+                </p>
+
+                <p>
+                    <strong>Department :</strong> {employee.department}
+                </p>
+
+                <p>
+                    <strong>Designation :</strong> {employee.designation}
+                </p>
+
+                <p>
+                    <strong>Address :</strong> {employee.address}
+                </p>
+
+            </div>
+
         </div>
 
 
